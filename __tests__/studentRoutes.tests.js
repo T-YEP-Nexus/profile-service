69d2842939728900d2e82babfa8eefc1dfc53b77
Lucie Aloccio <114408320+luciealc@users.noreply.github.com>
2025-09-02 16:59:15 +0200

    fix: student routes unit test changes

--- __tests__/studentRoutes.tests.js
+++ __tests__/studentRoutes.tests.js
@@ -3,8 +3,45 @@ const request = require('supertest');
 const BASE_URL = 'http://localhost:3004';
 
 let testStudentId = null;
+let testStudentUserProfile = null;
 
 describe('Student CRUD Routes (Integration)', () => {
+  describe('POST /student - Create new student', () => {
+    it('should create student successfully', async () => {
+      const newStudent = {
+        student_number: '123456',
+        major: 'Cloud',
+        // adapt the id_user_profile & promotion to an existing one in your test DB
+        id_user_profile: 55,
+        id_promotion: '388bf596-6be5-4fee-b227-38bab0d5ed4a'
+      };
+      const response = await request(BASE_URL).post('/student').send(newStudent);
+      expect(response.status).toBe(201);
+      expect(response.body.success).toBe(true);
+      expect(response.body.data.student_number).toBe(newStudent.student_number);
+
+      testStudentId = response.body.data.id;
+      testStudentUserProfile = response.body.data.id_user_profile;
+    });
+
+    it('should return 409 for existing student', async () => {
+      const response = await request(BASE_URL).post('/student').send({ 
+        student_number: '9276308067',
+        major: 'Cloud',
+        id_promotion: '388bf596-6be5-4fee-b227-38bab0d5ed4a',
+        id_user_profile: testStudentUserProfile
+      });
+      expect(response.status).toBe(409);
+    });
+
+    it('should return 400 for missing fields', async () => {
+      const response = await request(BASE_URL).post('/student').send({
+          promotion: '388bf596-6be5-4fee-b227-38bab0d5ed4a'
+      });
+      expect(response.status).toBe(400);
+    });
+  });
+
   describe('GET /students - Get all students', () => {
     it('should return all students successfully', async () => {
       const response = await request(BASE_URL).get('/students');
@@ -15,13 +52,11 @@ describe('Student CRUD Routes (Integration)', () => {
   });
 
   describe('GET /student/:id - Get student by ID', () => {
-    const validID = 14;
-
     it('should return advisor by valid ID', async () => {
-      const response = await request(BASE_URL).get(`/student/${validID}`);
+      const response = await request(BASE_URL).get(`/student/${testStudentId}`);
       expect(response.status).toBe(200);
       expect(response.body.success).toBe(true);
-      expect(response.body.data.id).toBe(validID);
+      expect(response.body.data.id).toBe(testStudentId);
     });
 
     it('should return 404 for non-existent advisor', async () => {
@@ -32,49 +67,14 @@ describe('Student CRUD Routes (Integration)', () => {
 
   describe('GET /student/profile/:id_user_profile - Get student by user profile id', () => {
     it('should return student by valid user profile id', async () => {
-      const userID = 14;
-      const response = await request(BASE_URL).get(`/student/profile/${userID}`);
+      const response = await request(BASE_URL).get(`/student/profile/${testStudentUserProfile}`);
       expect(response.status).toBe(200);
       expect(response.body.success).toBe(true);
-      expect(response.body.data.id_user_profile).toBe(userID);
+      expect(response.body.data.id_user_profile).toBe(testStudentUserProfile);
     });
 
     it('should return 400 for invalid user id', async () => {
       const response = await request(BASE_URL).get('/student/profile/invalidtype');
-      expect(response.status).toBe(400);
-    });
-  });
-
-  describe('POST /student - Create new student', () => {
-    it('should create student successfully', async () => {
-      const newStudent = {
-        student_number: '9276308067',
-        promotion: 'MSC_2026',
-        major: 'Cloud',
-        id_user_profile: 16
-      };
-      const response = await request(BASE_URL).post('/student').send(newStudent);
-      expect(response.status).toBe(201);
-      expect(response.body.success).toBe(true);
-      expect(response.body.data.student_number).toBe(newStudent.student_number);
-
-      testStudentId = response.body.data.id;
-    });
-
-    it('should return 409 for existing student', async () => {
-      const response = await request(BASE_URL).post('/student').send({ 
-        student_number: '9276308067',
-        promotion: 'MSC_2026',
-        major: 'Cloud',
-        id_user_profile: 16
-      });
-      expect(response.status).toBe(409);
-    });
-
-    it('should return 400 for missing fields', async () => {
-      const response = await request(BASE_URL).post('/student').send({
-          promotion: 'MSC_2028'
-      });
       expect(response.status).toBe(400);
     });
   });
@@ -85,7 +85,7 @@ describe('Student CRUD Routes (Integration)', () => {
       const response = await request(BASE_URL)
         .patch(`/student/${testStudentId}`)
         .send({ 
-          promotion: 'MSC_2028'
+          id_promotion: "388bf596-6be5-4fee-b227-38bab0d5ed4a"
         });
 
       expect(response.status).toBe(200);
